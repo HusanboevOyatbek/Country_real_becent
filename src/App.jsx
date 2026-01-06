62650fcf8703046b9d9bbf6d48d7ac648b912c43
frontend <husaboyevoyatbek@gmail.com>
2026-01-06 14:13:57 +0500

    bug/fixed Routes conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Layout } from 'antd'
 import React from 'react'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import CountrisePage from './page/countrise/CountrisePage'
-import CitysPage from './page/citys/CitysPage'
-import ToursPaje from './page/tours/ToursPaje'
-import HotelsPage from './page/hotels/HotelsPage'
-import DestPage from './page/destinations/destPage'
-import Layoutadmin from './components/Layout'
-import LoginPage from './page/login/LoginPage'
+import LoginPage from './page/login/LoginPage';
+import Layoutadmin from './components/Layout';
+import CountrisePage from './page/countrise/CountrisePage';
+import CitysPage from './page/citys/CitysPage';
+import ToursPaje from './page/tours/ToursPaje';
+import HotelsPage from './page/hotels/HotelsPage';
+import DestPage from './page/destinations/DestPage';
 
 const App = () => {
   const token = localStorage.getItem("token");
@@ -15,8 +15,8 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<LoginPage/>} />
-        <Route element={token ? <Layoutadmin /> : <Navigate to={"/"}/>}>
-          <Route path='countries' element={<CountrisePage />} />
+        <Route element={token ? <Layoutadmin/> : <Navigate to={"/"}/>}>
+          <Route path='countries' element={<CountrisePage/>} />
           <Route path='citys' element={<CitysPage />} />
           <Route path='tours' element={<ToursPaje />} />
           <Route path='hotels' element={<HotelsPage />} />
